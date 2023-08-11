59fab42b750668600d27739c6f0437db045c73cf
joao <joaorodrigues.009@outlook.com>
2023-08-11 20:09:35 -0300

    aula parte 1

---
+++ script.js
@@ -0,0 +1,17 @@
+
+//let chave = "chave1"
+//sessionStorage.setItem(chave, 'valor novo');
+
+//let lerChave = localStorage.getItem(chave);
+//console.log(lerChave)
+
+//let array=[1,2,3,4,5]
+//localStorage.setItem(array,JSON.stringify(array));
+
+//objeto
+//let dados ={nome:"Fiap", telefone:"11111"}
+//localStorage.setItem('info', JSON.stringify(dados))
+
+//criar lista de usuarios:
+let listadados=[]
+
