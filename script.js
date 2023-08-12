cef5f2511d0133c82fdca5518f4c09eef895b41e
joao <joaorodrigues.009@outlook.com>
2023-08-11 21:41:59 -0300

    finazado

--- script.js
+++ script.js
@@ -12,6 +12,34 @@
 //let dados ={nome:"Fiap", telefone:"11111"}
 //localStorage.setItem('info', JSON.stringify(dados))
 
-//criar lista de usuarios:
+//criar as listas de dados:
 let listadados=[]
+
+
+//criando as bases (Construtor):
+function Campos(campo1, campo2, campo3){
+    this.campo1 = campo1
+    this.campo2 = campo2
+    this.campo3 = campo3
+}
+
+//disparar o botao com o Dom:
+
+const btnEnviar = document.querySelector("#btnSumit");
+
+//evento click:
+btnEnviar.addEventListener('click', () =>{
+    const inputCampo1 = document.querySelector("#campo1");
+    const inputCampo2 = document.querySelector("#campo2");
+    const inputCampo3 = document.querySelector("#campo3");
+
+    
+    //instanciando o construtor
+    let info = new Campos(inputCampo1.value,inputCampo2.value,inputCampo3.value);
+
+    //lista de usuarios recebendo o objeto
+    listadados.push(info)
+    //armazenando a lista de usuarios convertidos em JSON
+    localStorage.setItem('dados',JSON.stringify(listadados))
+})
 
